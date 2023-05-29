559b25e216d708dc8e05da854341b25322d4f3a1
LucasCta <lucas.ct.araujo@gmail.com>
2023-05-29 03:42:13 -0300

    States and Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import MeuComponente from "./components/MeuComponente.jsx"
+import MeuContador from "./components/MeuContador.jsx"
 
-function App() {
+export default function App() {
 
   return (
     <div>
       <h1>Olá Mundo</h1>
-      <MeuComponente />
-      <MeuBotao conteudo='me clique'/>
-      <MeuBotao conteudo='depois aqui'/>
-      <MeuBotao conteudo='e por fim aqui'/>
+      <MeuContador />
     </div>
   )
 }
-
-function MeuBotao(props) {
-  return (
-    <button>{props.conteudo}</button>
-  )
-}
-
-export default App
--- src/components/MeuComponente.jsx
+++
@@ -1,5 +0,0 @@
-export default function MeuComponente() {
-  return ( 
-    <p>Meu primeiro paragrafo</p>
-  )
-}
